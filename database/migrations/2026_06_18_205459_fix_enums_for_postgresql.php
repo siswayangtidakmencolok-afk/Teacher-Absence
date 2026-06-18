cd46e12cc45cc62593828f7d1f0582f8360ef769
fhazwan <siswayangtidakmencolok@gmail.com>
2026-06-18 21:11:14 +0700

    Migrate MySQL ke PostgreSQL dan fix production setup

--- database/migrations/2026_06_18_205459_fix_enums_for_postgresql.php
+++ database/migrations/2026_06_18_205459_fix_enums_for_postgresql.php
@@ -4,14 +4,16 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::table('izins', function (Blueprint $table) {
-            $table->enum('status', ['belum', 'sudah', 'tolak'])->default('belum')->change();
+        Schema::create('none', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -20,6 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('none');
     }
 };
